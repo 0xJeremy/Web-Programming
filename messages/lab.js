ca108b48243d29899d11a1e19029c21985a98f6c
Jeremy <35907519+JeremyKanovsky@users.noreply.github.com>
2018-05-29 15:24:26 -0400

    Added Javascript and Styling

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,21 @@
-// Your JavaScript goes here...
+function parse() {
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if(this.readyState == 4 && this.status == 200) {
+			var jsonData = JSON.parse(this.responseText);
+			display_data(jsonData);
+		}
+	};
+	xmlhttp.open('GET', 'data.json', true);
+	xmlhttp.send();
+}
+
+function display_data(jsonData) {
+	var info = jsonData;
+	var messagetext = "";
+	for(var i in info)
+	{
+		messagetext += info[i].content + " " + info[i].username + "<br /><br />";
+	}
+	document.getElementById("messages").innerHTML = messagetext;
+}
